7f34466b6e0bc161e5b69cd8a71b9fa9d77e69a8
hafed <hafed.zouyed@univ-constantine2.dz>
2024-08-10 17:57:26 +0100

    adding sum for cart

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -2,17 +2,22 @@ import { StatusBar } from "expo-status-bar";
 import { View, Text, Platform, FlatList } from "react-native";
 import { useCart } from "@/providers/CartProvider";
 import CartListItem from "@/components/CartListItem";
+import Button from "@/components/Button";
 
 const CartScreen = () => {
-  const { items } = useCart();
+  const { items, total } = useCart();
   return (
-    <View>
+    <View className="p-2">
       <FlatList
         data={items}
         renderItem={({ item }) => <CartListItem cartItem={item} />}
         keyExtractor={(item) => item.id}
         contentContainerClassName="p-2 gap-2"
       />
+      <Text className="mt-4 text-center text-xl font-psemibold">
+        Total: ${total}
+      </Text>
+      <Button text="Checkout" />
 
       <StatusBar style={Platform.OS == "ios" ? "light" : "auto"} />
     </View>
