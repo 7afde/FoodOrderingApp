63005367113dbf942d84c92c6b019b999c3e1f7d
hafed <hafed.zouyed@univ-constantine2.dz>
2024-08-10 08:51:33 +0100

    initial expo ts nativewind app

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function TabOneScreen() {
+  return (
+    <View className="h-full items-center justify-center">
+      <Text className="my-2 font-psemibold">Pizza Peperoni</Text>
+      <Text className="text-light-tint font-pbold">$12.99</Text>
+    </View>
+  );
+}
