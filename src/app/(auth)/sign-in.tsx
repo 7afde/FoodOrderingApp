d45ee5063b44910fdc485f1cddbc640f28be8849
hafed <hafed.zouyed@univ-constantine2.dz>
2024-08-11 07:30:19 +0100

    adding auth pages (UI)

---
+++ src/app/(auth)/sign-in.tsx
@@ -0,0 +1,42 @@
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Link, Stack } from "expo-router";
+import Button from "@/components/Button";
+
+const SignInScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <View className="flex-1 justify-center px-4">
+      <Stack.Screen options={{ title: "Sign in" }} />
+
+      <Text className="text-gray-500 font-pregular">Email</Text>
+      <TextInput
+        value={email}
+        onChangeText={setEmail}
+        placeholder="jon@gmail.com"
+        className="bg-white p-4 rounded-md mt-2 mb-4"
+      />
+
+      <Text className="text-gray-500 font-pregular">Password</Text>
+      <TextInput
+        value={password}
+        onChangeText={setPassword}
+        placeholder=""
+        className="bg-white p-4 rounded-md mt-2 mb-4"
+        secureTextEntry
+      />
+
+      <Button text="Sign in" />
+      <Link
+        href="/sign-up"
+        className="text-light-tint mt-2 text-center underline underline-thickness:1 underline-offset:2 font-psemibold"
+      >
+        Create an account
+      </Link>
+    </View>
+  );
+};
+
+export default SignInScreen;
---
+++ src/app/(auth)/sign-up.tsx
@@ -0,0 +1,42 @@
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import Button from "@/components/Button";
+import { Link, Stack } from "expo-router";
+
+const SignUpScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <View className="flex-1 justify-center px-4">
+      <Stack.Screen options={{ title: "Sign up" }} />
+
+      <Text className="text-gray-500 font-pregular">Email</Text>
+      <TextInput
+        value={email}
+        onChangeText={setEmail}
+        placeholder="jon@gmail.com"
+        className="bg-white p-4 rounded-md mt-2 mb-4"
+      />
+
+      <Text className="text-gray-500 font-pregular">Password</Text>
+      <TextInput
+        value={password}
+        onChangeText={setPassword}
+        placeholder=""
+        className="bg-white p-4 rounded-md mt-2 mb-4"
+        secureTextEntry
+      />
+
+      <Button text="Create account" />
+      <Link
+        href="/sign-in"
+        className="text-light-tint mt-2 text-center underline underline-thickness:1 underline-offset:2 font-psemibold"
+      >
+        Sign in
+      </Link>
+    </View>
+  );
+};
+
+export default SignUpScreen;
